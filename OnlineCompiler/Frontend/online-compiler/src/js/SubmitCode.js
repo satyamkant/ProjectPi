8a9cea836eb552cbf78348876b90022228a2be3e
satyamkant <satyamkant2805@gmail.com>
2024-08-15 00:00:03 +0530

    update

--- OnlineCompiler/Frontend/online-compiler/src/js/SubmitCode.js
+++ OnlineCompiler/Frontend/online-compiler/src/js/SubmitCode.js
@@ -9,7 +9,7 @@ const SubmitCode = async (code, language, input) => {
 
   try {
       // Send POST request with the JSON body
-    const response = await axios.post('http://localhost:9000/api/code/execute', requestData, {
+    const response = await axios.post('http://localhost:1020/api/code/execute', requestData, {
       headers: {
         'Content-Type': 'application/json'
       }
