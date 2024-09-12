ba8d8823a4fe91198155bd00c4cec7bf4a1b1c3b
Satyam kant <68018658+satyamkant@users.noreply.github.com>
2024-09-12 21:22:12 +0530

    Update CodeExecutionController.java

--- OnlineCompiler/Backend/compilems/src/main/java/uk/satyampi/onlinecompiler/compilems/controller/CodeExecutionController.java
+++ OnlineCompiler/Backend/compilems/src/main/java/uk/satyampi/onlinecompiler/compilems/controller/CodeExecutionController.java
@@ -8,14 +8,13 @@ import uk.satyampi.onlinecompiler.compilems.service.CodeExecutionService;
 
 @RestController
 @RequestMapping("/api/code")
-@CrossOrigin(origins = "https://www.satyampi.uk")
+@CrossOrigin(origins = "*")
 public class CodeExecutionController {
 
     @Autowired
     private CodeExecutionService codeExecutionService;
 
     @PostMapping("/execute")
-    
     public ExecutionResult executeCode(@RequestBody CodeExecutionRequest request) {
         return codeExecutionService.executeCode(request.getCode(), request.getLanguage(), request.getInput());
     }
