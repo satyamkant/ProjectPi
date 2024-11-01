66a7b65a72e757520358f19c02d60b41cd1e1744
satyamkant <satyamkant2805@gmail.com>
2024-11-01 20:34:26 +0530

    personal portfolio website kickoff

--- OnlineCompiler/Backend/compilems/src/main/java/uk/satyampi/onlinecompiler/compilems/CompilemsApplication.java
+++ OnlineCompiler/Backend/compilems/src/main/java/uk/satyampi/onlinecompiler/compilems/CompilemsApplication.java
@@ -3,10 +3,15 @@ package uk.satyampi.onlinecompiler.compilems;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.File;
+import java.io.Serializable;
+import java.util.HashMap;
+
 @SpringBootApplication
 public class CompilemsApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(CompilemsApplication.class, args);
 	}
 
---
+++ OnlineCompiler/Backend/compilems/src/main/java/uk/satyampi/onlinecompiler/compilems/exception/OnlineCompilerException.java
@@ -0,0 +1,8 @@
+package uk.satyampi.onlinecompiler.compilems.exception;
+
+public class OnlineCompilerException extends Exception{
+    OnlineCompilerException(String message) {
+        super(message);
+    }
+
+}
