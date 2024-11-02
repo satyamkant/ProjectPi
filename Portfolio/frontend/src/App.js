fc9bd83c4053cff786fe297ceb7d6baa6253bb16
satyamkant <satyamkant2805@gmail.com>
2024-11-02 11:14:59 +0530

    adjusting color scheme for website

--- Portfolio/frontend/src/App.js
+++ Portfolio/frontend/src/App.js
@@ -1,16 +1,18 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 import './App.css';
 import Navbar from "./Components/Navbar";
 import Footer from "./Components/Footer";
 import Home from "./Components/Home";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
+import Resume from "./Components/Resume";
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
-      <Home/>
-      <Footer/>
+        <Navbar/>
+        <Home/>
+        <Resume/>
+         <Footer/>
     </div>
   );
 }
---
+++ Portfolio/frontend/src/Components/Resume.jsx
@@ -0,0 +1,11 @@
+import "./css/Resume.css"
+
+function Resume() {
+    return (
+        <section id="resume">
+            <div className="container"></div>
+        </section>
+    )
+}
+
+export default Resume;
