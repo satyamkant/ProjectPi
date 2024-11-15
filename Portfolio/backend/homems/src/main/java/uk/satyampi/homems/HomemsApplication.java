6ce4dafff4ee0aa531ec81f10d4dff108f045bae
satyamkant <satyamkant2805@gmail.com>
2024-11-15 16:39:19 +0530

    adding home ms backend first commit

---
+++ Portfolio/backend/homems/src/main/java/uk/satyampi/homems/HomemsApplication.java
@@ -0,0 +1,13 @@
+package uk.satyampi.homems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomemsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomemsApplication.class, args);
+	}
+
+}
---
+++ Portfolio/backend/homems/src/test/java/uk/satyampi/homems/HomemsApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.satyampi.homems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomemsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
