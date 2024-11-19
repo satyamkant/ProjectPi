ef14f3377bc084619ac0bdcf3b6d95bee9bf80f3
satyamkant <satyamkant2805@gmail.com>
2024-11-19 11:44:42 +0530

    removing blognavbar button

--- Portfolio/frontend/src/Components/Blog/BlogNavbar.jsx
+++ Portfolio/frontend/src/Components/Blog/BlogNavbar.jsx
@@ -9,8 +9,8 @@ function BlogNavbar() {
                 <div className="container-fluid">
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
                             data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                            aria-expanded="false" aria-label="Toggle navigation">f
-                        <span className="navbar-toggler-icon"></span>
+                            aria-expanded="false" aria-label="Toggle navigation">
+                        {/*<span className="navbar-toggler-icon"></span>*/}
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav mx-auto">
