d23b5f8c40cebff43d20567622ad60cf43078a5d
satyamkant <satyamkant2805@gmail.com>
2024-11-30 22:58:37 +0530

    adding actuator health to permit all

--- Portfolio/backend/SecurityMs/src/main/java/uk/satyampi/SecurityMs/config/SecurityConfig.java
+++ Portfolio/backend/SecurityMs/src/main/java/uk/satyampi/SecurityMs/config/SecurityConfig.java
@@ -50,6 +50,8 @@ public class SecurityConfig {
                         .permitAll()
                         .requestMatchers("/security/register")
                         .permitAll()
+                        .requestMatchers("/actuator/health")
+                        .permitAll()
                         .requestMatchers(new AntPathRequestMatcher("/security/admin"))
                         .hasRole("ADMIN")
                         .anyRequest()
